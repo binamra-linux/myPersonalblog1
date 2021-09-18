0d749e80e1da0ab2cecae07670e4971a5b9d2771
binamra-linux <53526396+binamra-linux@users.noreply.github.com>
2021-09-18 10:51:14 +0545

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get("/posts/:postName", function(req, res){
     }
   });
 });
-
-app.listen(3000, function() {
+var PORT = process.env.PORT;
+app.listen(PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
